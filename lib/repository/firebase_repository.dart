5e368ff562e017f3f8dd169f9bc054092ba8772b
Piotr Kozimor <p1996k@gmail.com>
2021-01-23 17:49:21 +0100

    feat: firebase crashlytics

--- lib/repository/firebase_repository.dart
+++ lib/repository/firebase_repository.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 
 class FirebaseRepository {
   FirebaseAuth auth;
@@ -8,6 +10,8 @@ class FirebaseRepository {
   CollectionReference users;
   FirebaseRepository() {
     Firebase.initializeApp().then((value) {
+      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
       auth = FirebaseAuth.instance;
       firestore = FirebaseFirestore.instance;
       users = FirebaseFirestore.instance.collection('users');
