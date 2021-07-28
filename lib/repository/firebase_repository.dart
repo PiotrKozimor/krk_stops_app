08314727ef2ce54644448e5416d4db5fb16d40bd
Piotr Kozimor <p1996k@gmail.com>
2021-07-28 21:34:00 +0200

    feat: remove crashlytics

--- lib/repository/firebase_repository.dart
+++ lib/repository/firebase_repository.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 
 class FirebaseRepository {
   late FirebaseAuth auth;
@@ -10,8 +8,6 @@ class FirebaseRepository {
   late CollectionReference<Map<String, dynamic>> users;
   FirebaseRepository() {
     Firebase.initializeApp().then((value) {
-      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
       auth = FirebaseAuth.instance;
       firestore = FirebaseFirestore.instance;
       users = FirebaseFirestore.instance.collection('users');
