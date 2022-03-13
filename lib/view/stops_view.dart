ccfe8985962aa2d0c4232d93d5380c6d1a7b287f
Piotr Kozimor <p1996k@gmail.com>
2022-03-13 22:21:20 +0100

    Strech stops

--- lib/view/stops_view.dart
+++ lib/view/stops_view.dart
@@ -11,7 +11,7 @@ class StopsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: stops
           .map((e) => InkWell(
                 child: Container(
