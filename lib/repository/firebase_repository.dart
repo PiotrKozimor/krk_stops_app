78b44bcad68e95d910d5a661f358e2368c8c0395
Piotr Kozimor <p1996k@gmail.com>
2024-10-09 21:07:34 +0200

    Bring up project to latest standards

--- lib/repository/firebase_repository.dart
+++ lib/repository/firebase_repository.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
 
 class FirebaseRepository {
   late FirebaseAuth auth;
@@ -10,7 +11,9 @@ class FirebaseRepository {
   Completer<void> initialized = Completer<void>();
   late CollectionReference<Map<String, dynamic>> users;
   FirebaseRepository() {
-    Firebase.initializeApp().then((value) {
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ).then((value) {
       auth = FirebaseAuth.instance;
       firestore = FirebaseFirestore.instance;
       users = FirebaseFirestore.instance.collection('users');
